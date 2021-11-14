e4a2ee5d2d95c32d08cf145904527f8cbc394471
ChrisW0524 <chriswang20050524@gmail.com>
2021-11-14 22:04:18 +0800

    Updated nav bar animation

--- main.js
+++ main.js
@@ -5,10 +5,24 @@ const showMenu = (toggleId, navId) =>{
 
     if(toggle && nav){ //if toggle and nav is not null
         toggle.addEventListener('click', ()=>{
-            console.log('clicked') //action when toggle is clicked
-            nav.classList.toggle('show')
+            console.log('clicked'); //action when toggle is clicked
+            nav.classList.toggle('show');
         })
     }
 }
 
+const removeMenu = () =>{
+    var elements = document.getElementsByTagName('a'); 
+    nav = document.getElementById('nav_menu');
+    for(var i=0; i<elements.length; i++){
+        if (elements[i].className == 'underline_effect') { 
+             elements[i].onclick = function(){ 
+               nav.classList.remove('show');
+       }
+     } 
+    }
+}
+
+
 showMenu('nav_toggle', 'nav_menu')
+removeMenu();
